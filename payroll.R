45e2c066dad841ffbe8e828ce618363b49de499c
theglucru <61841237+theglucru@users.noreply.github.com>
2021-01-04 16:51:11 -0500

    Payroll 2020 Plot

---
+++ payroll.R
@@ -0,0 +1,36 @@
+library(tidyverse)
+library(lubridate)
+library(readxl)
+library(modelr)
+
+# Reading
+# To create a table for the year
+#   week_beginning <-  seq(from = as.Date("2021-01-03"), to = as.Date("2021-12-31"), by = "1 week")
+# hours <- c(1:52)
+# cbind (week_beginning, hours)
+
+payroll <- read_xlsx("Book1.xlsx")
+
+# Formatting
+
+payroll$week_ending <- as.Date(payroll$week_ending) # Convert to date format
+payroll <- rename(payroll, OT_hours = `OT hours`) # Rename column
+payroll$OT <- ifelse(payroll$Hours > 40, "1", "0") # Indicates if OT or not
+payroll$OT_hours <- ifelse(payroll$OT_hours < 0, "0", payroll$OT_hours) # Remove negative OT hours
+
+# Base Plot
+plot <- payroll %>% 
+        ggplot(aes(x = week_ending))+
+        geom_ref_line(h = 40, size = 1)+
+        geom_line(aes(y = Hours))+
+        geom_point(aes(y = Hours, color = OT))
+
+# Graphics
+    plot + scale_x_date(breaks = payroll$week_ending, date_labels = "%m/%d")+
+            scale_y_continuous(breaks = seq(from = 10, to = 64, by = 10))+
+            scale_color_manual(values = c("black", "red"))+
+            labs(title = "Hours worked 2020")+
+            theme(axis.text.x = element_text(angle = -90, vjust=0.5),
+                  panel.grid.minor = element_blank(),
+                  legend.position = "none")
+            
